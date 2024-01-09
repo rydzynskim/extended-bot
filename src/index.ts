8b89196481dc837173aeb7fb4cbc8130407828e4
Mitch Rydzynski <mitch.rydzynski@gmail.com>
2024-01-08 22:49:40 -0500

    deleted comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import { run } from './state-machine/state-machine.js';
 
-// TODO:
-// - make the update function more pure (should not be doing anything besides updating state and spawning effects)
-// - consider type break up (show and clear history were a bit weird)
-
 run();
